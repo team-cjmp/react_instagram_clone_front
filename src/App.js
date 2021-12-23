0d8ea468411a874f0147efd60a6d183e92a7e9f3
suinautant <suinautant@gmail.com>
2021-12-23 20:51:08 +0900

    init App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h2>WELCOME !!</h2>
+        </div>
+    );
 }
 
 export default App;

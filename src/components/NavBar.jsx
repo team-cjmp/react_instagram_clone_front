a1654abb7ce56f7914d053fcc596b2f9e43e25d2
suinautant <suinautant@gmail.com>
2021-12-24 10:44:05 +0900

    사용 안 하는 파일 삭제

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,36 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 function Navbar() {
     const NavWrapper = styled.div`
-    width:100%;
-    height:60px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap:10px;
-    
-    `
+        width: 100%;
+        height: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+    `;
     const HomeLogo = styled.div`
-    flex:1;
-    `
+        flex: 1;
+    `;
     const NavSearch = styled.div`
-        flex:1;
-
-    `
+        flex: 1;
+    `;
     const NavIcons = styled.div`
-        flex:1;
-
-    `
+        flex: 1;
+    `;
     return (
         <>
-        <NavWrapper>
-            <HomeLogo>이미지</HomeLogo>
-            <NavSearch>검색</NavSearch>
-            <NavIcons>아이콘</NavIcons>
-        </NavWrapper>
+            <NavWrapper>
+                <HomeLogo>이미지</HomeLogo>
+                <NavSearch>검색</NavSearch>
+                <NavIcons>아이콘</NavIcons>
+            </NavWrapper>
         </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;

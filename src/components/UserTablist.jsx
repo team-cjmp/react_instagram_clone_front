f024cc1c70de977e2b25da6094a59c6e49ff70db
suinautant <suinautant@gmail.com>
2021-12-25 13:01:26 +0900

    사용자 페이지 컴포넌트 분리

---
+++ src/components/UserTablist.jsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+export class UserTablist extends PureComponent {
+  render() {
+    return (
+      <div>
+        <div>게시물</div>
+        <div>저장됨</div>
+        <div>태그됨</div>
+      </div>
+    );
+  }
+}
+
+export default UserTablist;

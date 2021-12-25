1246587dda97156b3966b387544364d546ac76c8
suinautant <suinautant@gmail.com>
2021-12-25 15:33:56 +0900

    styled UserMain

--- src/components/UserMain.jsx
+++ src/components/UserMain.jsx
@@ -8,14 +8,37 @@ export class UserMain extends PureComponent {
   render() {
     return (
       <div>
-        <main>
-          <UserProfile />
-          <UserTablist />
-          <UserArticle />
-        </main>
+        <Main>
+          <DivMain>
+            <UserProfile />
+            <UserTablist />
+            <UserArticle />
+          </DivMain>
+        </Main>
       </div>
     );
   }
 }
+
+const DivMain = styled.div`
+  align-items: stretch;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto 30px;
+  margin-bottom: 0;
+  max-width: 935px;
+  padding: 30px 20px 0 20px;
+  width: calc(100% - 40px);
+`;
+
+const Main = styled.main`
+  align-items: stretch;
+  background-color: rgba(var(--b3f, 250, 250, 250), 1);
+  display: flex;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+  position: relative;
+`;
 
 export default UserMain;

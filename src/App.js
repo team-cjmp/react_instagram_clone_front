01444cd934e8ec447fd3994457f97d85b5624d7f
suinautant <suinautant@gmail.com>
2021-12-29 14:19:20 +0900

    add file : init.css : css 스타일 우선 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import './init.css';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <h2>WELCOME ! START NOW !!</h2>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h2>WELCOME ! START NOW !!</h2>
+    </div>
+  );
 }
 
 export default App;
